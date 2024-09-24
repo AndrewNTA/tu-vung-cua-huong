b2cb3ae9a58fe6421d6ec5996ee01a8dde04ee1f
andrew nguyen <andrew.nta87@gmail.com>
2024-09-24 10:37:25 +0700

    simple UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react';
+import './styles.css';
 
 export function Home() {
-    return <>Home</>
+  return (
+    <div className="home-container">
+      <div className="adr-card">
+        <div className="adr-primary-text">School</div>
+        <div>(noun) /sku:l/</div>
+        <div>
+          <ul className='adr-ul'>
+            <li>
+              <div>I'm going to the school at six a.m morning every day</div>
+            </li>
+            <li>
+              <div>I love my school very much</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <button className="adr-button">Xem nghĩa</button>
+      </div>
+    </div>
+  );
 }
